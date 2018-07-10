61de945c78ac64e87b6e5e93a2923699de823dc6
znakki <znakki@gmail.com>
2018-07-10 16:56:27 +0300

    1. update course Service data

--- src/app/courses/courses.service.ts
+++ src/app/courses/courses.service.ts
@@ -9,39 +9,39 @@ export class CoursesService {
 
   public coursesItems: Course [] = [
     {
-      id: '3he3hj3hj3hj34j3h4j34h',
-      title: 'ANGULAR 6',
-      creation: '15/02/1990',
-      duration: 60,
-      description: 'FFFFFFFFFFFFFFFFFFFFFFFF FFFFFFFFFFFFFF'
+      id: '1234',
+      title: 'A Practical Guide to Algorithms with JavaScript',
+      creation: '2018-05-13',
+      duration: 45,
+      description: 'Learn to solve algorithms and analyze them efficiently in both an interview setting and also in your day-to-day development.',
     },
     {
-      id: '1a',
-      title: 'ANGULAR 6',
-      creation: '15/02/1990',
-      duration: 60,
-      description: 'klfjgklfsdjgkldjfgklsjkljskgsj'
+      id: '23456',
+      title: 'Webpack 4 Fundamentals',
+      creation: '2019-05-20',
+      duration: 247,
+      description: 'Learn the Webpack 4 plugin system, tour the Webpack source code and learn to build custom plugins and custom Webpack loaders.',
     },
     {
-      id: '2b',
-      title: 'ANGULAR 1.5 + Rails',
-      creation: '15/02/1940',
-      duration: 60,
-      description: 'flkjgfljgfsjgsfdjgksfjgklsfdjg'
+      id: '234765',
+      title: 'Complete Intro to Web Development, v2',
+      creation: '2018-09-15',
+      duration: 67,
+      description: 'More than an introduction, in this course you’ll go from building your first website to having the foundation for becoming a professional web developer!',
     },
     {
-      id: '3c',
-      title: 'PYTHON',
-      creation: '15/02/1990',
-      duration: 60,
-      description: 'sgjkdjgfsdkjgklsfdjgkjdfgklfdjgfd'
+      id: '644736343',
+      title: 'Testing React Applications',
+      creation: '2018-06-31',
+      duration: 98,
+      description: 'Fix errors before your app reaches the end user by writing maintainable unit test & integration tests for your React applications!',
     },
     {
-      id: '4d',
-      title: 'React',
-      creation: '15/02/1990',
-      duration: 60,
-      description: 'fgfdglkfjgklfdjgklafjgkfag'
+      id: '3435433',
+      title: 'SQL Fundamentals',
+      creation: '2017-04-20',
+      duration: 23,
+      description: 'Learn SQL, the most popular language for storing, querying and analyzing the relational data that powers your server-side applications.',
     }
   ];
 
